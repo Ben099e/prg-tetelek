4e0e85e0204382dd665547ac8be9b7f8b6da1b0c
bajkobe19 <bajkobe19@nsjg.eu>
2022-10-15 09:15:23 +0200

    Add main method el

--- src/hu/tutorial/App.java
+++ src/hu/tutorial/App.java
@@ -1,15 +1,18 @@
 package hu.tutorial;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class App {
 
-    private static int[] numbers;
+    private static List<Integer> numbers;
 
     public static void main(String[] args) {
         System.out.println("Progrmozási tételek megvalósítása");
         numbers = init();
-        print();
+        print(numbers);
         System.out.println("A sorozat összege: " + summation());
         int divisior = 3;
         boolean isExist = decision(divisior);
@@ -17,45 +20,70 @@ public class App {
             if (isExist) {
                 System.out.println("A sorozat " + (selectiom(divisior) + 1 ) + ". eleme osztható " + divisior + "-val.");
             }
+        List<Integer> sortedList = sort(numbers);
+        print(sortedList);
     }
 
-    private static int[] init() {
-        int[] nums = new int[100];
+    private static List<Integer> init() {
+        List<Integer> nums = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = random.nextInt(99) + 1;
+        for (int i = 0; i < 100; i++) {
+            nums.add(random.nextInt(99) + 1);
         }
         return nums;
     }
 
-    private static void print() {
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.printf("%4d", numbers[i]);
+    private static void print(List<Integer> nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.printf("%4d", nums.get(i));
         }
         System.out.println();
     }
-
+/*
     private static int summation() {
         int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
+        for (int i = 0; i < numbers.size(); i++) {
+            sum += numbers.get(i);
         }
         return sum;
     }
+*/
+private static int summation() {
+    return numbers.stream()
+            .mapToInt(i -> i)
+            .sum();
+}
 
     private static boolean decision(int divisor) {
         int i = 0;
-        while (i < numbers.length && !(numbers[i] % divisor == 0)) {
+        while (i < numbers.size() && !(numbers.get(i) % divisor == 0)) {
             i++;
         }
-        return i < numbers.length;
+        return i < numbers.size();
     }
 
     private static int selectiom(int divisor) {
         int i = 0;
-        while (!(numbers[i] % divisor == 0)) {
+        while (!(numbers.get(i) % divisor == 0)) {
             i++;
         }
         return i;
     }
+/*    private static void sort() {
+        for (int i = 0; i < numbers.length -1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    int p = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = p;
+                }
+            }
+        }
+    }*/
+   private static List<Integer> sort(List<Integer> nums) {
+       return nums.stream()
+               .sorted()
+               .collect(Collectors.toList());
+   }
+
 }
